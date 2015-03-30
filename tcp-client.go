4fb346174bc17739dcd444f0be57808979d46950
Bruno Roberto Zanuzzo <brunorobertozanuzzo@gmail.com>
2015-03-30 19:36:22 -0300

    including logs

--- tcp-client.go
+++ tcp-client.go
@@ -4,23 +4,27 @@ import "net"
 import "fmt"
 import "bufio"
 import "os"
+import "log"
 
 func main() {
 
-	// connect to this socket
-	conn, _ := net.Dial("tcp", "127.0.0.1:8081")
+   // connect to this socket
+   conn, err := net.Dial("tcp", "127.0.0.1:8081")
+   if err != nil{ log.Fatal(err); return }
 
-	for {
-		// read in input from stdin
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Text to send: ")
-		text, _ := reader.ReadString('\n')
+   for {
+      // read in input from stdin
+      reader := bufio.NewReader(os.Stdin)
+      fmt.Print("Text to send: ")
+      text, err := reader.ReadString('\n')
+      if err != nil{ log.Fatal(err); break }
 
-		// send to socket
-		fmt.Fprintf(conn, text+"\n")
+      // send to socket
+      fmt.Fprintf(conn, text+"\n")
 
-		// listen for reply
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		fmt.Print("Message from server: " + message)
-	}
+      // listen for reply
+      message, err := bufio.NewReader(conn).ReadString('\n')
+      if err != nil{ log.Fatal(err); break }
+      fmt.Print("Message from server: " + message)
+   }
 }
